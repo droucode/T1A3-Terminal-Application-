c5d57fc91bf34875f1a4b6387b47c90339caad0e
droucode <iamdrou@gmail.com>
2021-09-24 11:20:52 +1000

    testing basic rubycode for timer/no gem

---
+++ index.rb
@@ -0,0 +1,19 @@
+# pass the number of seconds as the parameter
+
+seconds = eval(ARGV[0]).to_i
+start_time = Time.now
+
+loop do
+  elapsed = Time.now - start_time
+  print "\e[D" * 17
+  print "\033[K"
+
+  if elapsed > seconds
+    puts "Time's up!"
+    exit
+  end
+
+  print Time.at(seconds - elapsed).utc.strftime('%H:%M:%S.%3N')
+  sleep(0.05)
+end
+ 
