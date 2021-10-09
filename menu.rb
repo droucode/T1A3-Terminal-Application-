3538a363d49def9582d78976a0debbac03969e66
droucode <iamdrou@gmail.com>
2021-10-09 23:25:31 +1100

    menu.rb/ added a while loop for user interation (name)/ methods.rb/ created this file to hold methods used throughout the appliction

--- menu.rb
+++ menu.rb
@@ -4,17 +4,45 @@ require 'timers'
 require 'tty-prompt'
 require 'date'
 #---------Required Files-----#
-require_relative 'ascii.rb'
+# require_relative 'ascii.rb'
 require_relative 'energy.rb'
 require_relative 'mindfulness.rb'
+require_relative 'methods.rb'
 # require_relative 'test_alarm.rb'
 #--------Calling Classes-----#
 energy_routine = Energy.new
 mindfulness_routine = Mindfulness.new 
 # clock = Test.new
 
+#------Welcome message and directions-------#
+
+logo
+welcome
+name_attempt = 0 
+while name_attempt < 2
+    name = gets.strip.capitalize 
+    if name !=""
+        print "\e[2J\e[f"
+        puts "(*bellows)Righto #{name} pick one of these routines and you'll be on your way. See you soon!"
+        break 
+    else 
+        name_attempt +=1
+        if name == ""
+            puts "Grrrromp, I didn't catch your name:"
+        end 
+        if name_attempt ==2 
+            print "\e[2J\e[f"
+            puts "i'll call you Al Jr. then, hows that? Pick one of these routines and you'll be on your way. See you soon"
+            name = "Al jr"
+        end 
+    end 
+end 
+
+
+
 #-------Main Menu---------#
-#WHILE LOOP - user can cycle throug the menu options. Exit menu by selecting Exit 
+# WHILE LOOP - user can cycle throug the menu options. Exit menu by selecting Exit 
+
 
 while true 
     notice = Pastel.new.black.on_bright_yellow.detach
@@ -33,33 +61,15 @@ while true
     #runs energise routine
     energy_routine.timers 
 
+  
     when "Mindfulness Routine"
     #runs mindfulnes routine 
     mindfulness_routine.timers 
-
+ 
+ 
     when "Exit"
-
-        main_menu
+    main_menu
+    puts "So long #{name}, come by the lake anytime, i'm always willing to crunch through anything with you.".colorize(:green) 
         break 
     end 
 end 
-
-
-
-
-
-
-
-
-# if main_menu == 'Energise Routine'
-#     energy_routine.timers 
-
-    
-# elsif main_menu == 'Mindfulness Routine'
-#     mindfulness_routine.timers 
-
-# elsif main_menu == "Set Alarm"
-
-# else 
-#     puts ""
-# end 
--- methods.rb
+++ methods.rb
@@ -1,3 +1,6 @@
+#------Logo------#
+
+def logo 
 crunchtime = "
 
                 ─────▄████▀█▄
@@ -10,3 +13,17 @@ crunchtime = "
     █▄▄ █▀▄ █▄█ █░▀█ █▄▄ █▀█ ░░ ░█░ █ █░▀░█ ██▄ 
     "
 puts crunchtime.light_yellow  
+end 
+
+#------clearing terminal between functions------#
+
+def auto_clear 
+    print "\e[2J\e[f"
+end 
+
+#-------Headings-------#
+
+def welcome 
+    puts "(*bellows...*)Oh HEY, WHATS YOU NAME? My name's Al, soon you'll be crunching through your loadload just like we ol aligators do!".colorize(:cyan)
+end 
+
